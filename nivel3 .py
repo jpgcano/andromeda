9721727778467311912fa872242dc7c6f9b2aa98
cadena0 <cadenajuanjose68@gmail.com>
2025-11-07 13:32:21 -0500

    atualizacion del nivel3

--- nivel3 .py
+++ nivel3 .py
@@ -1,18 +1,18 @@
-"""#-------- primer punto--------
+#-------- primer punto--------
 #utilice el bucle for para que al imprimer los numeros del 1 al 11
 #al principio pobre del 0 al 9 pero no era lo que me pedian 
 #la i es una variable que va a tomar cada valor del rango y la puedo indicar en el print para señalar lo que quiero que salga en la terminal 
 for i in range (1, 11): 
     print("este es el numero:", i)
-"""
-"""#--------segundo punto--------
+
+#--------segundo punto--------
 # use n como variable para pedir un numero al usuario
 #y en el rango le sume 1 para que llegue hasta el numero ingresado por el usuario
 n = int(input("ingrese un numero: "))
 for i in range ( 1+n):
      print("este es el numero:", i)
-"""
-"""#--------tercer punto--------
+
+#--------tercer punto--------
 #utilice n como variable para pedir un numero al usuario
 #y luego utilice un bucle for para multiplicar ese numero por los numeros del 0 al 10
 n = int(input("ingrese un numero para multiplicar: "))
@@ -21,18 +21,22 @@ for i in range  (0, 11):
     multiplicacion = n * i
     print("este es el numero: ", n, "x", i, "=", multiplicacion)
     
-"""
-"""#--------cuarto punto-------- 
+
+#--------cuarto punto-------- 
+#utilice el bucle while para que cuente de 10 a 0
+#y la i la inicie en 10 para que empiece desde ahi y luego le reste 1 hasta que llegue a 0
 i= 10
 while i >= 0:
     print(i)
     i -= 1
-"""
-"""#--------quinto punto--------
 
+#--------quinto punto--------
+
+#utilice el modulo random para generar un numero aleatorio entre 1 y 10
 import random 
 print ("Adivina el numero entre 1 y 10")
 numero_secreto = int(random.randint(1, 10))
+#utilice un bucle while para que el usuario pueda seguir intentando adivinar el numero hasta que lo logre
 while True:
     adivina = int(input("ingresa tu numero: "))
     if numero_secreto > adivina:
@@ -42,9 +46,11 @@ while True:
     if numero_secreto == adivina:
         print("Felicidades, has adivinado el numero!")
         break
-"""
 
-#-------sexto punto--------
+
+#-------sexto punto-------
+#utilice estas variables para mas adelante poder sumar los numeros ingresados por el usuario
+#y la while true para que el usuario pueda ingresar numeros hasta que ingrese un 0 y ahi se detenga el bucle
 suma= 0 
 numero = 0 
 while True:
