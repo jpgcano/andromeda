d8f8ef7d0ba4e62fb9076fc4c3a95b5ae51cbe60
Jpgcano <juan-0395@hotmail.com>
2025-11-07 06:37:41 -0500

    primer ejercicio de nivel 1

---
+++ taller_basico.py
@@ -0,0 +1,16 @@
+"""
+    Taller de Consolidación – Ruta Básica de Python
+"""
+
+"""
+    Nivel 1 – Fundamentos y variables
+    Objetivo: Practicar tipos de datos, entrada y salida, concatenación y operaciones básicas.
+"""
+
+"""
+    Hola usuario: pide al usuario su nombre y edad. Luego imprime un mensaje: "Hola [nombre], tienes [edad] años."
+"""
+
+nombre  = input ("por favor Ingrese su nombre: ")
+edad    = int   (input("Por favor ingrese su edadd: "))
+print   (f"Hola {nombre}, tienes {edad} años")
