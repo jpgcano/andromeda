2945d4cbe2df8af2f839963c7f4bdf388fdaaea7
Jpgcano <juan-0395@hotmail.com>
2025-11-12 13:05:41 -0500

    esqueleto del proyecto

---
+++ ClinicManager.py
@@ -0,0 +1,16 @@
+
+
+pacientes ={}
+def RegistrarPacientes():
+    historial =[]
+    dianostico=[]
+    informacionInicial=()
+    return
+def BuscarPacientes():
+    return
+def ActualizarPAcientes(): ### Primero ahcer el buscar pacientes
+    return
+def ElininarPacientes():
+    return
+def Reporte():
+    return
