03e9825cdd3dc2d6c450586176f62d968da2b9b9
cadena0 <cadenajuanjose68@gmail.com>
2025-11-12 13:54:34 -0500

    actualizacion

--- ClinicManager.py
+++ ClinicManager.py
@@ -1,7 +1,23 @@
 
 
 pacientes ={}
-def RegistrarPacientes():
+def RegistrarPacientes ():
+    while True:
+        print ("--REGISTRO DE PACIENTES--")
+        print ("1. Ingresa tu numero de CC: ")
+        print ("2. Ingresa tu nombre completo: ")
+        print ("3. Ingresa tu edad: ")
+        print ("4. Ingresa tu genero: ")
+        
+        opcion = input("Elige una opcion (1-4): ")
+        
+        if opcion == "1":
+            nombre = input("ingrese tu nombr: ")
+                           
+                       
+                       
+                       
+        
     historial =[]
     dianostico=[]
     informacionInicial=()
