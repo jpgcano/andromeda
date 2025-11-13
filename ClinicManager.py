da91c8181475aab2c66e844a5802e5e1541300e3
Jpgcano <juan-0395@hotmail.com>
2025-11-13 13:37:58 -0500

    eliminando reporte

--- ClinicManager.py
+++ ClinicManager.py
@@ -134,25 +134,7 @@ def EliminarPacientes():
 
     return
 def Reporte():
-    print("**********************************************")
-    print("     Bienvenido a la sección de reportes     ")
-    menu = f"""
-                Ingrese opción de reporte: 
-                [1] Todos los pacientes registrados
-                [2] Pacientes mayores de 60 años
-                [3] Diagnósticos más frecuentes
-                [4] Cantidad total de pacientes
-            """
-    if menu ==1 and isinstance(menu, int):
-        for paciente in pacientes:
-            print (f"""
-        "id":       {paciente["id"]},
-        "nombre":   {paciente["id"]},                ,
-        "edad":    {paciente["id"]},
-        "genero":  {paciente["id"]},   
-        "diagnostico":{paciente["id"]},
-        "historial":{paciente["id"]},
-                    """)
+    return
 
 #RegistrarPacientes()
 #BuscarPacientes()
